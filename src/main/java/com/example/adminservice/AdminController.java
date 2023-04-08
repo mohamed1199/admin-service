7b346155cf78a35d3eab19a3ae27de0c8c5d5af6
mohamed nebbai <nebbai1999@gmail.com>
2023-04-08 22:39:16 +0000

    add env for user-service endpoint

--- src/main/java/com/example/adminservice/AdminController.java
+++ src/main/java/com/example/adminservice/AdminController.java
@@ -1,5 +1,9 @@
 package com.example.adminservice;
 
+import org.apache.juli.logging.Log;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.env.Environment;
 import org.springframework.http.*;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,27 +14,37 @@ import java.io.IOException;
 import java.net.InetSocketAddress;
 import java.net.Socket;
 import java.util.Collections;
+import java.util.Map;
 
 
 @RestController
 @RequestMapping("/api/admin-service")
 public class AdminController {
 
+    @Value("${USER_SERVICE_ENDPOINT}:default_endpoint")
+    String userServiceEndpoint ;
+
     @GetMapping("/")
     public String getDefault(){
         return "Hello from Admin Service - " + getAddress() ;
     }
+
 
     @GetMapping("/admins")
     public String getHello(){
         return "I have 5 admins - " +  getAddress() ;
     }
 
+    @GetMapping("/env")
+    public String getEnvVars(){
+        return userServiceEndpoint ;
+    }
+
     @GetMapping("/connect")
     public String connect(){
         RestTemplate restTemplate = new RestTemplate();
 
-        String uri = "http://user-service:8082/api/user-service/"; // or any other uri
+        String uri = "http://"+ userServiceEndpoint + "/api/user-service/"; // or any other uri
 
         HttpHeaders headers = new HttpHeaders();
         headers.setAccept(Collections.singletonList(MediaType.APPLICATION_JSON));
